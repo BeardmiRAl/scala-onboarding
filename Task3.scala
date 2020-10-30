cb6d61fa09bf7c216040e3ce8b7976bd4a59da2d
Елагин Роман <elagin@kinoplan.ru>
2020-10-30 12:00:47 +0300

    done without If and reverse usage

--- Task3.scala
+++ Task3.scala
@@ -1,15 +1,18 @@
 /*Дана строка,
  содержащая прописные/строчные буквы алфавита и символы пробела.
- Верните длину последнего слова в строке. Если одно слово вывести 0*/
+ Верните длину последнего слова в строке. Если последнее слово не существует вывести 0*/
 
 object Task3 extends App {
   def solution(s: String) = {
-    if (s.contains(" ")) {
-      val resd = s.reverse.split(" ")
-      resd(0).length
+    val p = s.split(" ").lastOption.toList
+    p.length match {
+      case 0 => 0
+      case _ => p.head.length
     }
-    else 0
-  }
+    }
+
+
+
 
   println(s"Task 3 = ${Task3.solution("Hello World")}") //5
 }
